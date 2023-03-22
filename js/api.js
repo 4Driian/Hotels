ee5ce26bbf44c7acb014fc6fa557862189c2e79f
Ian Lara <108307887+larace@users.noreply.github.com>
2023-03-22 12:55:05 -0600

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -44,10 +44,35 @@ fetch(`${API}/Hotel`)
 fetch(`${API}/Hotel`)
 .then((response) => response.json())
 .then((Hotel) =>{
+//   let tmpl = Hotel.map((hotels, index) => 
+//   `<h2 class="hotels${index}">${hotels.DDescription}</h2>`)´
+  let HTML_tmpl = document.querySelector("#info01");
+  let HTML_tmpl1 = document.querySelector("#heroimg1");
+  let HTML_tmpl2 = document.querySelector("#gallery_yun1")
+  let HTML_tmpl3 = document.querySelector("#trivago1")
+  HTML_tmpl.innerHTML = `<h2>${Hotel[1].DDescription}</h2>`
+  HTML_tmpl1.innerHTML = `<img class="hero__img" src="${Hotel[1].ImageDestiny}" alt="...">
+  <h1 class="center__name">${Hotel[1].Destiny}</h1>`
+  HTML_tmpl2.innerHTML = ` <img src="${Hotel[1].Imagegallery}" alt="...">
+  <img src="${Hotel[1].imageg2}" alt="...">
+  <img src="${Hotel[1].imageg3}" alt="...">`
+  HTML_tmpl3.innerHTML = `<a href="tama-hotel-one.html">
+  <button class="hotel-3">
+    <span class="titulo-boton">Hotel Name</span>
+  </button>
+</a>
+<a href="tama-hotel-two.html">
+  <button class="hotel-4">
+    <span class="titulo-boton">Hotel Name</span>
+  </button>
+</a>`
+});
+
+fetch(`${API}/Hotel`)
+.then((response) => response.json())
+.then((Hotel) =>{
   let HTML_tmpl4 = document.querySelector("#hero_image");
-  let HTML_tmpl5 = document.querySelector("#slider-top");
   let heroimage2= Hotel[0].HeroimageD
-  let slider = Hotel[0].ImageDS
   HTML_tmpl4.innerHTML = `  
   <img class="img-home" src="${heroimage2}" alt="">
   <img class="img-home-mobile" src="assets/img/destinations/home-img-header-mobile.jpg" alt="">
@@ -72,6 +97,20 @@ fetch(`${API}/Hotel`)
     let room_image = Hotel[0].ImageHotel
     let hotel_name = Hotel[0].Hotelname
     let hotel_text = Hotel[0].Hoteldes
+    HTML_tmpl6.innerHTML = `<img class="hero__img" src="${room_image}" alt="...">
+    <h2 class="center__hotel-name">${hotel_name}</h2>`
+    HTML_tmpl7.innerHTML = `<h2>${hotel_name}</h2>
+    <p>${hotel_text}</p>`
+});
+
+fetch(`${API}/Hotel`)
+.then((response) => response.json())
+.then((Hotel) =>{
+    let HTML_tmpl6 = document.querySelector("#hero_room2");
+    let HTML_tmpl7 = document.querySelector("#space-text2")
+    let room_image = Hotel[1].ImageHotel
+    let hotel_name = Hotel[1].Hotelname
+    let hotel_text = Hotel[1].Hoteldes
     HTML_tmpl6.innerHTML = `<img class="hero__img" src="${room_image}" alt="...">
     <h2 class="center__hotel-name">${hotel_name}</h2>`
     HTML_tmpl7.innerHTML = `<h2>${hotel_name}</h2>
@@ -109,6 +148,46 @@ fetch(`${API}/Room`)
       <h3><span class="features__price-discount ">${price}</span></h3>
     </div>
   </div>
+  </div>
+  <div class="features__container">
+  <div class="features__img">
+    <img src="${Room[1].roomimg}" alt="">
+  </div>
+  <div class="features__img">
+    <img src="${Room[1].roomimg2}" alt="">
+    <img src="${Room[1].roomimg3}" alt="">
+  </div>
+  <div class="features__info">
+    <h2>${Room[1].roomname}</h2> <br>
+    <p>${Room[1].roomdes}</p> <br>
+    <div class="features__info-prices">
+      <ul>
+        <li>${Room[1].roomser}</li>
+      </ul>
+      <h3><span class="features__price">${Room[1].discount}</span></h3>
+      <h3><span class="features__price-discount ">${Room[1].price}</span></h3>
+    </div>
+  </div>
+  </div>
+  <div class="features__container">
+  <div class="features__img">
+    <img src="${Room[2].roomimg}" alt="">
+  </div>
+  <div class="features__img">
+    <img src="${Room[2].roomimg2}" alt="">
+    <img src="${Room[2].roomimg3}" alt="">
+  </div>
+  <div class="features__info">
+    <h2>${Room[2].roomname}</h2> <br>
+    <p>${Room[2].roomdes}</p> <br>
+    <div class="features__info-prices">
+      <ul>
+        <li>${Room[2].roomser}</li>
+      </ul>
+      <h3><span class="features__price">${Room[2].discount}</span></h3>
+      <h3><span class="features__price-discount ">${Room[2].price}</span></h3>
+    </div>
+  </div>
   </div>`
 
 });
@@ -128,56 +207,56 @@ fetch(`${API}/Hotel`)
     HTML_tmpl10.innerHTML = `      <div class="slide">
     <img class="slider__img" src="${destiny_1}" alt="">
     <div>
-      <a href="desti-one.html" class="slider-destination">Santa Teresa</a>
+      <a href="desti-one.html" class="slider-destination">${Hotel[0].Destiny}</a>
     </div>
   </div>
 
   <div class="slide">
     <img class="slider__img" src="${destiny_2}" alt="">
     <div>
-      <a href="desti-two.html" class="slider-destination">Tamarindo</a>
+      <a href="desti-two.html" class="slider-destination">${Hotel[1].Destiny}</a>
     </div>
   </div>
 
   <div class="slide">
     <img class="slider__img" src="${destiny_3}" alt="">
     <div>
-      <a href="desti-three.html" class="slider-destination">Poás</a>
+      <a href="desti-three.html" class="slider-destination">${Hotel[2].Destiny}</a>
     </div>
   </div>
 
   <div class="slide">
     <img class="slider__img" src="${destiny_4}" alt="">
     <div>
-      <a href="desti-four.html" class="slider-destination">Papagayo</a>
+      <a href="desti-four.html" class="slider-destination">${Hotel[3].Destiny}</a>
     </div>
   </div>
 
   <div class="slide">
     <img class="slider__img" src="${destiny_5}" alt="">
     <div>
-      <a href="desti-five.html" class="slider-destination">Manzanillo</a>
+      <a href="desti-five.html" class="slider-destination">${Hotel[4].Destiny}</a>
     </div>
   </div>
 
   <div class="slide">
     <img class="slider__img" src="${destiny_6}" alt="">
     <div>
-      <a href="desti-six.html" class="slider-destination">Fortuna</a>
+      <a href="desti-six.html" class="slider-destination">${Hotel[5].Destiny}</a>
     </div>
   </div>
 
   <div class="slide">
     <img class="slider__img" src="${destiny_7}" alt="">
     <div>
-      <a href="#" class="slider-destination">Jaco</a>
+      <a href="#" class="slider-destination">${Hotel[6].Destiny}</a>
     </div>
   </div>
 
   <div class="slide">
     <img class="slider__img" src="${destiny_8}" alt="">
     <div>
-      <a href="#" class="slider-destination">Manuel Antonio</a>
+      <a href="#" class="slider-destination">${Hotel[7].Destiny}</a>
     </div>
   </div>
   <div class="control__buttons-container">
